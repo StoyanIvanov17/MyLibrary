ffcce693d28a239768cabe34076f291168cb0bd3
StoyanIvanov17 <stoyanivanovmail@gmail.com>
2024-09-20 04:56:17 +0300

    did some work on templates & refactored some stuff on the models

--- library/library/lb_accounts/forms.py
+++ library/library/lb_accounts/forms.py
@@ -1,16 +1,7 @@
-from django import forms
+
 from django.contrib.auth import forms as auth_forms, get_user_model
 
 UserModel = get_user_model()
-
-
-class LibraryCardAuthenticationForm(auth_forms.AuthenticationForm):
-    username = forms.CharField(
-        label='Library Card Number',
-    )
-
-    def clean_username(self):
-        return self.cleaned_data['username']
 
 
 class LibraryUserCreationForm(auth_forms.UserCreationForm):
