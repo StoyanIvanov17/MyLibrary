0b0580bc3b69409e9cd81721e7899570f58cd95f
StoyanIvanov17 <stoyanivanovmail@gmail.com>
2024-09-29 02:28:52 +0300

    started working on the event functionalities

--- library/library/lb_events/forms.py
+++ library/library/lb_events/forms.py
@@ -6,7 +6,17 @@ from library.lb_events.models import Event
 class EventBaseForm(forms.ModelForm):
     class Meta:
         model = Event
-        fields = ('name', 'date', 'location', 'description', 'age_group')
+        fields = ('name', 'date', 'time', 'location', 'age_group', 'event_image', 'description')
+
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Name'}),
+            'date': forms.DateInput(attrs={'placeholder': 'Date', 'type': 'date'}),
+            'time': forms.TimeInput(attrs={'placeholder': 'Time', 'type': 'time', 'step': '900'}),
+            'location': forms.TextInput(attrs={'placeholder': 'Location'}),
+            'age_group': forms.TextInput(attrs={'placeholder': 'Age Group'}),
+            'event_image': forms.FileInput(attrs={'placeholder': 'Event Image'}),
+            'description': forms.Textarea(attrs={'placeholder': 'Description'}),
+        }
 
 
 class EventCreateForm(EventBaseForm):
