b74d1729adefd62fba7cf8d25df03c433fd08751
StoyanIvanov17 <stoyanivanovmail@gmail.com>
2024-10-01 22:09:57 +0300

    added deletion funcitonalities and some JS function reworks

--- library/library/lb_collections/urls.py
+++ library/library/lb_collections/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('<int:pk>/<slug:slug>/', include([
         path('detail/', views.ItemDetailView.as_view(), name='item detail'),
         path('edit/', views.ItemEditView.as_view(), name='item edit'),
+        path('delete/', views.ItemDeleteView.as_view(), name='item delete'),
         path('save_item/', views.save_item_view, name='save_item'),
     ])),
 ]
